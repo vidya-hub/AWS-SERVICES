d1c5dcadcb138461241ef6ad506781d153745e43
vidya-hub <mavuduru.123.sagar@gmail.com>
2021-03-02 19:15:57 +0530

    final commit

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def detect_faces_from_localfile(photo):
     return response
 
 def main():
-    photo='sample.jpg'
+    photo='test.jpg'
     faceslist=[]
     image = Image.open(open(photo,'rb'))
     iwidth,iheight=image.size
